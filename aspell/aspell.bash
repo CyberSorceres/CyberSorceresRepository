6438c5ba84487b3867ba23a8a988db4463862dfe
Andrea Rezzi <andrea.rezzi@studenti.unipd.it>
2023-11-14 11:51:39 +0100

    Aggiunto dizionario inglese

--- aspell/aspell.bash
+++ aspell/aspell.bash
@@ -2,6 +2,9 @@
 
 # Get aspell output...
 <"$1" aspell pipe list -t --lang=it_IT --personal=./.aspell.ignore |
-grep '[a-zA-Z]\+ [0-9]\+ [0-9]\+' -oh | \
-grep '[a-zA-Z]\+' -o | \
+    grep '[a-zA-Z]\+ [0-9]\+ [0-9]\+' -oh | \
+    grep '[a-zA-Z]\+' -o | \
+    aspell pipe list -t --lang=en_US |
+    grep '[a-zA-Z]\+ [0-9]\+ [0-9]\+' -oh | \
+    grep '[a-zA-Z]\+' -o | \
 while read word; do grep -on "\<$word\>" $1; done
